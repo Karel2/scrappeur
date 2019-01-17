50b3587b9d1c9d562a6642d2b6a1a9ede80e17c6
Karel2 <cloareckarel@gmail.com>
2019-01-17 17:33:14 +0100

    Ajout du 1er exercice

---
+++ lib/trader.rb
@@ -0,0 +1,23 @@
+	require 'open-uri'
+	require 'nokogiri'
+
+	currencies_name = []
+	currencies_value = []
+	doc = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))
+
+	doc.css('tr > td.text-left.col-symbol').each do |node|
+		currencies_name << node.text
+	end
+
+	doc.css('tr > td.no-wrap.text-right > a.price').each do |node|
+		currencies_value << node.text
+	end
+
+	currencies_value.each_index do |place| 
+		currencies_value[place] = currencies_value[place].delete_prefix('$').to_f
+	end	
+
+	hash_crypto = currencies_name.zip(currencies_value).to_h
+
+	print hash_crypto
+
